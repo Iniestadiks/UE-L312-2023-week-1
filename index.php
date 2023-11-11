dba617e42ff91614f484ab6ed81b095fa645aa38
Madicke GUEYE <gueyemadicke0751@gmail.com>
2023-11-11 09:13:48 +0100

    Update index.php with new content

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-
 declare(strict_types=1);
+?>
 
-echo 'test';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>UE-L312 PHP Example</title>
+</head>
+<body>
+    <h1>Welcome to the UE-L312 Project</h1>
+    <p>This is a simple PHP example for the UE-L312 course.</p>
+    
+    <section>
+        <h2>Current Date and Time</h2>
+        <p>
+            <?php
+            echo 'Current date and time is: ' . date('Y-m-d H:i:s');
+            ?>
+        </p>
+    </section>
+</body>
+</html>
